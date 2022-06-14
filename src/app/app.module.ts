6453619baaa345f160f3ad13691a045e0ced9e40
pushpa0679 <bora.pushpanjali@gmail.com>
2022-06-14 12:00:27 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { NgifComponent } from './ng-if.component';
-import { SimpleComponent } from './simple/simple.component';
-import { ContextComponent } from './context/context.component';
-import { InputParentComponent } from './input-parent/input-parent.component';
-import { InputChildComponent } from './input-parent/input-child.component';
-import { NgContentChildParentComponent } from './ng-content-child-parent/ng-content-child-parent.component';
-import { NgContentChildChildComponent } from './ng-content-child-parent/ng-content-child-child.component';
-import { Sample1Component } from './sample1.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    NgifComponent,
-    SimpleComponent,
-    ContextComponent,
-    InputParentComponent,
-    InputChildComponent,
-    NgContentChildParentComponent,
-    NgContentChildChildComponent,
-    Sample1Component,
-  ],
+  declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
