1319568a69f76d2e3640b46025320739a3afb48b
pushpa0679 <bora.pushpanjali@gmail.com>
2022-06-16 15:54:12 +0530

    1 file modified, 1 added, 1 deleted

--- src/app/form-2/form-2.component.ts
+++ src/app/form-2/form-2.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MustMatch } from '../helpers/must-match-validator';
+import { MustMatch } from '../_helpers/must-match-validator';
 
 @Component({
   selector: 'app-form-2',
